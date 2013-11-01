9470b4b1f6f2937c4b9be38cca201e825cf8ffd6
paul.harvey <csirac2@gmail.com>
2013-11-01 15:16:56 +1100

    make minimum Module::Build 0.28 for Centos 5.0

--- lib/Mobile/Wurfl/SQL.pm
+++ lib/Mobile/Wurfl/SQL.pm
@@ -29,30 +29,6 @@ CREATE INDEX IF NOT EXISTS user_agent_idx
 CREATE INDEX IF NOT EXISTS id ON device (id);
 
 HERE
-    , 'generic' => <<'HERE'
--- Don't forget to re-run script/update_Mobile-Wurfl-SQL.pl script if you modify
-DROP TABLE IF EXISTS capability;
-CREATE TABLE capability (
-        name varchar(255) NOT NULL DEFAULT '',
-        value varchar(255) DEFAULT '',
-        groupid varchar(255) NOT NULL DEFAULT '',
-        deviceid varchar(255) NOT NULL DEFAULT '',
-        ts DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-CREATE INDEX IF NOT EXISTS groupid ON capability (groupid);
-CREATE INDEX IF NOT EXISTS name_deviceid ON capability (name, deviceid);
-DROP TABLE IF EXISTS device;
-CREATE TABLE device (
-        user_agent varchar(255) NOT NULL DEFAULT '',
-        actual_device_root varchar(255),
-        id varchar(255) NOT NULL DEFAULT '',
-        fall_back varchar(255) NOT NULL DEFAULT '',
-        ts DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-CREATE INDEX IF NOT EXISTS user_agent ON device (user_agent);
-CREATE INDEX IF NOT EXISTS id ON device (id);
-
-HERE
     , 'mysql' => <<'HERE'
 -- Don't forget to re-run script/update_Mobile-Wurfl-SQL.pl script if you modify
 DROP TABLE IF EXISTS capability;
@@ -76,6 +52,30 @@ CREATE TABLE device (
     KEY user_agent (user_agent),
     KEY id (id)
 ) ENGINE=InnoDB;
+
+HERE
+    , 'generic' => <<'HERE'
+-- Don't forget to re-run script/update_Mobile-Wurfl-SQL.pl script if you modify
+DROP TABLE IF EXISTS capability;
+CREATE TABLE capability (
+        name varchar(255) NOT NULL DEFAULT '',
+        value varchar(255) DEFAULT '',
+        groupid varchar(255) NOT NULL DEFAULT '',
+        deviceid varchar(255) NOT NULL DEFAULT '',
+        ts DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX IF NOT EXISTS groupid ON capability (groupid);
+CREATE INDEX IF NOT EXISTS name_deviceid ON capability (name, deviceid);
+DROP TABLE IF EXISTS device;
+CREATE TABLE device (
+        user_agent varchar(255) NOT NULL DEFAULT '',
+        actual_device_root varchar(255),
+        id varchar(255) NOT NULL DEFAULT '',
+        fall_back varchar(255) NOT NULL DEFAULT '',
+        ts DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX IF NOT EXISTS user_agent ON device (user_agent);
+CREATE INDEX IF NOT EXISTS id ON device (id);
 
 HERE
     , 'sqlite' => <<'HERE'
